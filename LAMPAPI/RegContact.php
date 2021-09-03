27429fca8766885e6464d2acc208b6f727f8150f
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2021-09-02 21:57:45 -0400

    RegContact added and working =)!

---
+++ LAMPAPI/RegContact.php
@@ -0,0 +1,47 @@
+<?php
+	$inData = getRequestInfo();
+    $FirstName = $inData["FirstName"];
+    $LastName = $inData["LastName"];
+	$ContactPhoto = $inData["ContactPhoto"];
+	$UserID = $inData["UserID"];
+	$Email = $inData["Email"];
+	$PhoneNumber = $inData["PhoneNumber"];
+	$Address =  $inData["Address"];
+	$Relationship =  $inData["Relationship"];
+	$Notes =  $inData["Notes"];
+	$conn = new mysqli("localhost", "TheBeast", "UCFGROUP14ROCKS", "FINAL");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserID, FirstName, LastName, ContactPhoto, 
+								 Email, PhoneNumber, Address, Relationship, Notes)
+								VALUES(?,?,?,?,?,?,?,?,?);");
+		$stmt->bind_param("issssssss", $UserID, $FirstName, $LastName, $ContactPhoto, $Email, 
+						  $PhoneNumber, $Address, $Relationship, $Notes);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
