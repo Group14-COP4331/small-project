aa793bb10e18541923cfc544e83bd48fa2bb295d
nateotis <nathaneotis@gmail.com>
2021-09-04 01:13:39 -0400

    Search API Endpoint
    Created the Search API endpoint (documentation on SwaggerHub)

---
+++ LAMPAPI/Search.php
@@ -0,0 +1,70 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "UCFGROUP14ROCKS", "FINAL"); 	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT FirstName, LastName, ContactPhoto FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID=?");
+		$contactName = "%" . $inData["Search"] . "%";
+		$stmt->bind_param("sss", $contactName, $contactName, $inData["UserId"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '{"FirstName": "' . $row["FirstName"]  . '", "LastName": "' . $row["LastName"] . '", "ContactPhoto": "' . $row["ContactPhoto"] . '"}';
+			//$encodedResults .= json_encode($searchResults); 
+		}
+		
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '], "error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
