7261bbab9e2cfc952c3a33aa4bce6f173e64c557
ken3512 <89816670+ken3512@users.noreply.github.com>
2021-09-04 16:04:08 -0400

    Updated Login, deletecontact, and contactinfo APIs

---
+++ DeleteContact.php
@@ -0,0 +1,36 @@
+<?php
+	$inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "TheBeast", "UCFGROUP14ROCKS", "FINAL");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE Name=?  AND UserID=?");
+		$stmt->bind_param("si", $inData["Name"], $inData["UserID"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
