8e9d4116a0dd1714c33dd6983f26066ed4dd892b
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2021-09-08 23:08:41 -0400

    Added EditPassword.php

---
+++ LAMPAPI/EditPassword.php
@@ -0,0 +1,37 @@
+<?php
+	$inData = getRequestInfo();
+	$userID = $inData["UserID"];
+	$password = $inData["Password"];
+	$conn = new mysqli("localhost", "TheBeast", "UCFGROUP14ROCKS", "FINAL");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Users SET Password = ? WHERE ID = ?");
+		$stmt->bind_param("si", md5($password), $userID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
